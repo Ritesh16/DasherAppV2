ac89d58d3bd7f8371a51fe614d5cdde11af72cb8
Ritesh16 <ritesh.sharma.cse@gmail.com>
2023-10-08 11:05:40 -0400

    Adding config and help class

---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+export const API_URL = "http://localhost/DasherAPI/api/";
+export const TIME_OUT = 10;
---
+++ src/js/helper.js
@@ -0,0 +1,31 @@
+import { TIME_OUT } from "./helper";
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+export const callAPI = async function (url, uploadData = undefined) {
+  try {
+    const fetchPromise = uploadData
+      ? fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(uploadData),
+        })
+      : fetch(url);
+
+    const result = await Promise.race([fetchPromise, timeout(TIME_OUT)]);
+    const data = await result.json();
+
+    if (!result.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
