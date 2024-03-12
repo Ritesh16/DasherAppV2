2f62798451916d8fa8387641d0bef60985cd33d5
Ritesh16 <ritesh.sharma.cse@gmail.com>
2024-03-11 22:05:03 -0400

    Adding pagination view

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -3,28 +3,52 @@ import View from "./view.js";
 class PaginationView extends View {
   _parentElement = document.querySelector(".pagination");
 
-  addHandlerRender(handler) {
-    window.addEventListener("load", handler);
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener("click", function (e) {
+      e.preventDefault();
+      console.log(3);
+      const btn = e.target.closest(".page-link");
+      console.log(2, e.target);
+      console.log(1, btn);
+      if (!btn) return;
+
+      const goto = +btn.dataset.goto;
+      console.log(goto);
+      handler(goto);
+    });
   }
 
   _generateMarkup() {
-    const currentPage = this._data.state.headers.currentPage;
-    const lastPage = this._data.state.headers.totalPages;
-
+    const headers = JSON.parse(this._data.state.headers);
+    console.log(this._data);
+    const currentPage = headers.currentPage;
+    const lastPage = headers.totalPages;
     return `
     <div>
         <ul class="pagination">
           <li class="page-item disabled">
             <a class="page-link" href="#">&laquo;</a>
           </li>
-    ${Array(lastPage)
+    ${Array(10)
       .fill(1)
       .map((_, i) => {
-        return `    
+        if (currentPage == i + 1) {
+          return `    
           <li class="page-item active">
-            <a class="page-link" href="#">${i}</a>
+            <button data-goto=${i + 1} class="page-link"> <span>${
+            i + 1
+          }</span></button>
           </li>          
       `;
+        } else {
+          return `    
+          <li class="page-item">
+          <button data-goto=${i + 1} class="page-link"> <span>${
+            i + 1
+          }</span></button>
+          </li>          
+      `;
+        }
       })
       .join("")}
 
